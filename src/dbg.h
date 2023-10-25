54f8162a9a82d54d5b026f21a9bd84641791660f
treeswift <treeswift@mailbox.org>
2023-10-25 12:21:30 -0500

    "dbg.h" needs <stdio.h>

--- src/dbg.h
+++ src/dbg.h
@@ -2,6 +2,8 @@
 #define _FATCTL_SRC_DBG_H_
 
 #ifdef _FATCTL_DEBUG
+#include <stdio.h>
+
 #define _FATCTL_LOG(format, ...) { fprintf(_FATCTL_DEBUG, format "\n", ##__VA_ARGS__); fflush(_FATCTL_DEBUG); }
 #else
 #define _FATCTL_LOG(...)
